e63c81b77de5297cce2821fb26fbfaa0201a4409
Dasha6elka <CB4kot@gmail.com>
2019-03-24 16:23:01 +0300

    Последовательность

---
+++ 10.12/10.12/main.cpp
@@ -0,0 +1,98 @@
+﻿/*
+10.12. Последовательность 4 (6)
+Имеется последовательность целых чисел x 1 , x 2 , …, x n . Найти количество троек чисел, в каждой
+из которых одно из чисел равно сумме двух других.
+Ввод из файла INPUT.TXT. В первой строке содержится значение n (3 ≤ n ≤ 5000). Во второй
+строке заданы через пробел значения x 1 , x 2 , …, x n (1  x i  10 5 ).
+Вывод в файл OUTPUT.TXT. В единственной строке выводится количество указанных троек
+чисел (x i , x j , x k ).
+Щеглова Дарья ПС-23
+Microsoft Visual C++ 2017
+Версия 15.9.6
+*/
+
+#include "pch.h"
+#include <vector>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <unordered_map>
+
+const std::string input_path = "input.txt";
+const std::string output_path = "output.txt";
+
+void write_numbers_in_vector(std::istream& input, std::vector<int>& numbers);
+
+std::unordered_map<int, int> fill_map(std::vector<int> numbers);
+
+int count_triplets(std::vector<int>& numbers, std::unordered_map<int, int> numbers_map, size_t count);
+
+void main()
+{
+	std::ifstream input(input_path);
+	std::ofstream output(output_path);
+	if (!input.is_open())
+	{
+		throw std::runtime_error("Cannot open file" + input_path);
+	}
+
+	std::vector<int> numbers;
+	size_t count;
+
+	input >> count;
+	numbers.reserve(count * 4);
+	write_numbers_in_vector(input, numbers);
+
+	std::sort(numbers.rbegin(), numbers.rend(), [](const int a, const int b) {return a > b; });
+	auto numbers_map = fill_map(numbers);
+
+	auto triplets_amount = count_triplets(numbers, numbers_map, count);
+	output << triplets_amount << std::endl;
+}
+
+void write_numbers_in_vector(std::istream& input, std::vector<int>& numbers)
+{
+	int number;
+	while (input >> number)
+	{
+		numbers.push_back(number);
+	}
+}
+
+std::unordered_map<int, int> fill_map(std::vector<int> numbers)
+{
+	std::unordered_map<int, int> numbers_map;
+	for (auto i : numbers)
+	{
+		++numbers_map[i];
+	}
+	return numbers_map;
+}
+
+int count_triplets(std::vector<int>& numbers, std::unordered_map<int, int> numbers_map, const size_t count)
+{
+	auto triplets_amount = 0;
+	int two_numbers_sum;
+	const auto max_number = numbers[count - 1];
+	for (size_t i = 0; i < count; ++i)
+	{
+		if ((i != count - 1) && ((numbers[i] + numbers[i + 1])) > max_number)
+		{
+			break;
+		}
+		for (auto j = i + 1; j < count; ++j)
+		{
+			two_numbers_sum = numbers[i] + numbers[j];
+			if (two_numbers_sum > max_number)
+			{
+				break;
+			}
+			auto result = numbers_map.find(two_numbers_sum);
+			if (result != numbers_map.end())
+			{
+				triplets_amount += result->second;
+			}
+		}
+	}
+	return triplets_amount;
+}
