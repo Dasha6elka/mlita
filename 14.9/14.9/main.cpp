a6d813ee1f086c06078e59d90e50eb3d7589316f
Dasha6elka <CB4kot@gmail.com>
2019-04-14 14:16:20 +0300

    Правки

--- 14.9/14.9/main.cpp
+++ 14.9/14.9/main.cpp
@@ -16,12 +16,13 @@ Microsoft Visual C++ 2017
 const int months_in_year = 12;
 const int start_year = 1901;
 const int end_year = 2099;
+std::ifstream input("input.txt");
+std::ofstream output("output.txt");
+
+void search_friday_13(int first_year, int second_year, std::string months[], const int days_in_months[]);
 
 int main()
 {
-	std::ifstream input("input.txt");
-	std::ofstream output("output.txt");
-
 	 std::string months[12] = {
 		"JANUARY", "FEBRUARY", "MARCH", 
 		"APRIL", "MAY", "JUNE", 
@@ -34,6 +35,34 @@ int main()
 	int second_year;
 	input >> first_year >> second_year;
 
+	search_friday_13(first_year, second_year, months, days_in_months);
+}
+
+void friday_13(const int week_day, const int day, const int year, const int first_year, std::string months[], const int month)
+{
+	if ((week_day == 5) && (day == 13) && (year >= first_year))
+	{
+		output << year << " " << months[month - 1].data() << std::endl;
+	}
+}
+
+void fun_leap_year(const int month, const int day, const int leap_year, int & week_day)
+{
+	if ((leap_year == 4) && (month == 2) && (day == 28))
+	{
+		if (week_day == 7)
+		{
+			week_day = 1;
+		}
+		else
+		{
+			week_day++;
+		}
+	}
+}
+
+void search_friday_13(const int first_year, const int second_year, std::string months[], const int days_in_months[])
+{
 	auto week_day = 2;
 	auto leap_year = 1;
 
@@ -43,24 +72,25 @@ int main()
 		{
 			for (auto day = 1; day <= days_in_months[month - 1]; ++day)
 			{
-				if ((week_day == 5) && (day == 13) && (year >= first_year))
-					output << year << " " << months[month - 1].data() << std::endl;
+				friday_13(week_day, day, year, first_year, months, month);
 				if (week_day == 7)
-					week_day = 1;
-				else
-					week_day++;
-				if ((leap_year == 4) && (month == 2) && (day == 28))
 				{
-					if (week_day == 7)
-						week_day = 1;
-					else
-						week_day++;
+					week_day = 1;
 				}
+				else
+				{
+					week_day++;
+				}
+				fun_leap_year(month, day, leap_year, week_day);
 			}
 		}
 		if (leap_year == 4)
+		{
 			leap_year = 1;
+		}
 		else
+		{
 			++leap_year;
+		}
 	}
 }
