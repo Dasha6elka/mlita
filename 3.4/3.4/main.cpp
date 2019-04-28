eec4aedd1935e6b29c81ceab1cc9db70d4e1292b
Dasha6elka <CB4kot@gmail.com>
2019-04-28 18:54:33 +0300

    К-ичные числа

---
+++ 3.4/3.4/main.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include <vector>
+#include "fstream"
+
+std::ifstream input("input.txt");
+std::ofstream output("output.txt");
+
+int main() {
+	long num1, num2;
+	input >> num1 >> num2;
+
+	std::vector<long> arr(200);
+	arr[0] = 1;
+	arr[1] = num2 - 1;
+
+	auto i = 2;
+	while (i <= num1) {
+		arr[i] = (arr[i - 1] + arr[i - 2]) * (num2 - 1);
+		++i;
+	}
+
+	output << arr[num1];
+}
